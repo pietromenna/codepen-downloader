3835de0244f84a13a143f88e08bd847b765d7f49
Frederico <fred.rbittencourt@gmail.com>
2016-03-07 07:25:27 -0300

    progress bar while downloading

--- src/cpen.js
+++ src/cpen.js
@@ -3,6 +3,7 @@
 let fs = require('fs');
 let http = require('http');
 let async = require('async');
+let ProgressBar = require('progress');
 
 const BASE_URL = "http://codepen.io"
 
@@ -21,18 +22,25 @@ module.exports = {
       .on('err', (err) => {
         fn(err);
       })
-    }).on('error', (e) => {
-      fn(e);
     });
+
   },
 
   create : (err, result) => {
     if(err) {
       console.error(`Error: ${err.message}`);
     } else {
-      fs.appendFile('example/index.html', result.html, (e) => { if(e) console.error(e.message) });
-      fs.appendFile('example/style.css', result.css, (e) => { if(e) console.error(e.message) });
-      fs.appendFile('example/main.js', result.js, (e) => { if(e) console.error(e.message) });
+      fs.appendFile('example/index.html', result.html, (e) => {
+        if(e) console.error(e.message);
+      });
+
+      fs.appendFile('example/style.css', result.css, (e) => {
+        if(e) console.error(e.message);
+      });
+
+      fs.appendFile('example/main.js', result.js, (e) => {
+        if(e) console.error(e.message);
+      });
     }
 
   }
--- src/interface.js
+++ src/interface.js
@@ -4,21 +4,44 @@ let program    = require('commander');
 let async      = require('async');
 let fs         = require('fs');
 let cpen       = require('./cpen');
+let ProgressBar = require('progress');
 
 module.exports = program;
 
 let download = function(url, options) {
+
+  let progress = new ProgressBar(`Downloading ( :percent )[:bar]`, {
+    complete: '=',
+    incomplete: '-',
+    width: 50,
+    total: 4
+  });
+
+  progress.tick();
   async.parallel({
     html : (callback) => {
-      cpen.download(url, 'html', callback);
+      cpen.download(url, 'html', (err, data) => {
+        progress.tick();
+        callback(err,data);
+      });
     },
     css : (callback) => {
-      cpen.download(url, 'css', callback)
+      cpen.download(url, 'css', (err, data) => {
+        progress.tick();
+        callback(err,data);
+      });
     },
     js : (callback) => {
-      cpen.download(url, 'js', callback);
+      cpen.download(url, 'js', callback, (err, data) => {
+        progress.tick();
+        callback(err,data);
+      });
     }
-  }, cpen.create);
+  }, (err, results) => {
+    progress.tick();
+    console.log('creating files');
+    cpen.create(err, results);
+  });
 }
 
 program
