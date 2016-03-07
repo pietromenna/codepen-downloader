5ad3ae04b4610094c100bb254be340d0ac28f8c3
Frederico <fred.rbittencourt@gmail.com>
2016-03-06 22:31:35 -0300

    added tests

--- src/cpen.js
+++ src/cpen.js
@@ -2,6 +2,7 @@
 
 let fs = require('fs');
 let http = require('http');
+let async = require('async');
 
 const BASE_URL = "http://codepen.io"
 
@@ -11,25 +12,29 @@ module.exports = {
     http.get(`${BASE_URL}${url}.${file}`, (res) => {
       let buffer = '';
       res
-        .on('data', (chunk) => {
-          buffer += chunk;
-        })
-        .on('end', () => {
-          fn(null, buffer);
-        })
-        .on('err', (err) => {
-          fn(err);
-        })
+      .on('data', (chunk) => {
+        buffer += chunk;
+      })
+      .on('end', () => {
+        fn(null, buffer);
+      })
+      .on('err', (err) => {
+        fn(err);
+      })
     }).on('error', (e) => {
       fn(e);
     });
   },
 
   create : (err, result) => {
-    if(err) console.error(`Error: ${err.message}`);
-    fs.appendFile('example/index.html', result.html, (e) => { if(e) console.error(e.message) });
-    fs.appendFile('example/style.css', result.css, (e) => { if(e) console.error(e.message) });
-    fs.appendFile('example/main.js', result.js, (e) => { if(e) console.error(e.message) });
+    if(err) {
+      console.error(`Error: ${err.message}`);
+    } else {
+      fs.appendFile('example/index.html', result.html, (e) => { if(e) console.error(e.message) });
+      fs.appendFile('example/style.css', result.css, (e) => { if(e) console.error(e.message) });
+      fs.appendFile('example/main.js', result.js, (e) => { if(e) console.error(e.message) });
+    }
+
   }
 
 }
