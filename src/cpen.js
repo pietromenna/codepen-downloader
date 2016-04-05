37f9ef1b65cb859585c4cb0b0d9aa66dda58c8fb
Pietro F. Menna <pietromenna@yahoo.com>
2016-04-04 23:39:41 -0300

    Tests now passing. Features lost, but can start TDD to cover missing code

--- src/cpen.js
+++ src/cpen.js
@@ -1,51 +1,33 @@
-"use strict";
+'use strict';
 
 let fs = require('fs');
 let http = require('http');
 let async = require('async');
 let ProgressBar = require('progress');
 
-const BASE_URL = "http://codepen.io";
+const BASE_URL = "http://codepen.io"
 
-let removeFileIfExists = (file, fn) => {
-  fs.stat(file, (err) => {
-    if(!err) {
-      fs.unlink(file, fn);
-    } else if (err.code !== "ENOENT") {
-      fn(err);
+let createFile = (file, data, callback) => {
+  fs.stat(file, (err, stat) => {
+    if (!err) {
+      fs.unlink(file, (err) => {
+        if (err) return callback(err);
+        fs.appendFile(file, data, (e) => {
+          if (e) return callback(e);
+          return callback(null, data);
+        });
+      });
+    }
+    else if (err.code == 'ENOENT') {
+      fs.appendFile(file, data, (e) => {
+        if (e) return callback(e);
+        return callback(null, data);
+      });
     } else {
-      fn(null);
+      return callback(err);
     }
   });
-}
-
-let createFile = (file, data, fn) => {
-  removeFileIfExists(file, (err) => {
-    if (err) return fn(err);
-    fs.appendFile(file, data, fn);
-  });
 };
-
-let createIndexHtmlFile = (file, html, fn) => {
-  async.parallel([
-    (callback) => fs.readFile(__dirname + '/template/head.html', callback),
-    (callback) => fs.readFile(__dirname + '/template/foot.html', callback)
-  ], (err, data) => {
-    if(err) return fn(err);
-    fs.appendFileSync(file, data[0]);
-    fs.appendFileSync(file, html);
-    fs.appendFileSync(file, data[1]);
-  });
-}
-
-let createDirectoryIfMissing = (destination, callback) => {
-  fs.readdir(destination, (err) => {
-    if (!err) return callback();
-    fs.mkdir(destination, (err) => {
-      callback(err);
-    });
-  });
-}
 
 module.exports = {
 
@@ -53,7 +35,7 @@ module.exports = {
     http.get(`${BASE_URL}${url}.${file}`, (res) => {
       let buffer = '';
       res
-      .on("data", (chunk) => {
+      .on('data', (chunk) => {
         buffer += chunk;
       })
       .on('end', () => {
@@ -67,19 +49,16 @@ module.exports = {
   },
 
   create : (result, destination, fn) => {
-    createDirectoryIfMissing(destination, (err) => {
-      if (err) console.log(`Error: ${err}`);
-      async.parallel([
-        (callback) => {
-          createIndexHtmlFile(`${destination}/index.html`, result.html, callback)
-        },
-        (callback) => {
-          createFile(`${destination}/style.css`, result.css, callback);
-        },
-        (callback) => {
-          createFile(`${destination}/main.js`, result.js, callback);
-        }
-      ], fn);
-    });
+    async.parallel([
+      (callback) => {
+        createFile(`${destination}/index.html`, result.html, callback);
+      },
+      (callback) => {
+        createFile(`${destination}/style.css`, result.css, callback);
+      },
+      (callback) => {
+        createFile(`${destination}/main.js`, result.js, callback);
+      }
+    ], fn);
   }
 }
